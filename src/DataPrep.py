95f006681807e9da084d9823b7f8574c0c0d8585
konstantinosKokos <konstantinos@riseup.net>
2019-01-20 20:55:28 +0100

    first commit

---
+++ src/DataPrep.py
@@ -0,0 +1,121 @@
+import sys
+import os
+sys.path.append(os.path.join(os.getcwd(), '..'))
+from LassyExtraction.src import WordType
+from LassyExtraction.src.utils.PostProcess import indexize, get_unique
+import pickle
+
+sys.modules['src.WordType'] = WordType
+
+from typing import Iterable, Dict, Set, TypeVar, Callable, Tuple
+import subprocess
+import io
+
+import torch
+from torch import Tensor
+from functools import reduce
+from torch.utils.data import Dataset
+
+from tqdm import tqdm
+
+
+T1 = TypeVar('T1')
+T2 = TypeVar('T2')
+
+
+def load(x: str='data/XYZ.p'):
+    with open(x, 'rb') as f:
+        X, Y, Z = pickle.load(f)
+    return X, Y, Z
+
+
+def map_type_sequence_to_index_sequence(ts: Iterable[WordType.WordType], d: Dict[WordType.WordType, int]) \
+        -> Iterable[int]:
+    return tuple(map(lambda x: Tensor([d[x]]), ts))
+
+
+def map_sequences_to_sequences(T: Iterable[Iterable[T1]], d: Dict[T1, T2],
+                               fun: Callable[[Iterable[T1], Dict[T1, T2]], Iterable[T2]]) \
+        -> Iterable[Iterable[T2]]:
+    return tuple(map(lambda x: fun(x, d), T))
+
+
+def map_word_to_tensor(w: str, d: Dict[str, Tensor]) -> Tensor:
+    subwords = w.split()
+    subtensors = list(map(lambda x: d[x] / Tensor([len(subwords)]), subwords))
+    return reduce(lambda x, y: x+y, subtensors).unsqueeze(0)
+
+
+def map_word_sequence_to_tensor_sequence(ws: Iterable[str], d: Dict[str, Tensor]) -> Iterable[Tensor]:
+    return tuple(map(lambda x: map_word_to_tensor(x, d), ws))
+
+
+def write_vocab(voc: Set[str], voc_file: str) -> None:
+    subwords = []
+    with open(voc_file, 'w') as f:
+        for word in voc:
+            for subword in word.split():
+                if subword not in subwords:
+                    subwords.append(subword)
+                    f.write(subword + '\n')
+
+
+def vectorize_words(X: Iterable[Iterable[str]], fastText_path: str, model_path: str, storage_path: str,
+                    vocab_path: str) -> None:
+    vocabulary = get_unique(X)
+    write_vocab(vocabulary, vocab_path)
+    with open(storage_path, 'w') as f:
+        ft = subprocess.Popen([fastText_path, 'print-word-vectors', model_path],
+                              stdin=open(vocab_path), stdout=f)
+        ft.wait()
+        f.flush()
+
+
+def load_vectors(fname: str) -> Dict[str, Tensor]:
+    ret = dict()
+    fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+    for line in tqdm(fin.readlines()):
+        if len(line.split()) == 2:
+            continue
+        tokens = line.rstrip().split(' ')
+        ret[tokens[0]] = Tensor(list(map(float, tokens[1:])))
+    return ret
+
+
+def do_everything():
+    fastText_path = '/home/kokos/Documents/Projects/FastText/fastText-0.1.0/fasttext'
+    model_path = '/home/kokos/Documents/Projects/FastText/fastText-0.1.0/models/wiki.nl.bin'
+    storage_path = 'data/vectors.vec'
+    vocab_path = 'data/vocabulary.voc'
+    X, Y, Z = load()
+
+    type_to_int = indexize(get_unique(Y).union(set(Z)))
+    type_to_int[None] = 0
+    type_to_int['SOS'] = len(type_to_int) + 1
+    # int_to_type = {v: k for k, v in type_to_int.items()}
+    type_sequences = map_sequences_to_sequences(Y, type_to_int, map_type_sequence_to_index_sequence)
+    results = map_type_sequence_to_index_sequence(Z, type_to_int)
+
+    if not os.path.isfile(vocab_path):
+        vectorize_words(X, fastText_path, model_path, storage_path, vocab_path)
+    word_to_vec = load_vectors(storage_path)
+
+    vector_sequences = map_sequences_to_sequences(X, word_to_vec, map_word_sequence_to_tensor_sequence)
+
+    return TLGDataset(vector_sequences, type_sequences, results, type_to_int)
+
+
+class TLGDataset(Dataset):
+    def __init__(self, X: Iterable[Iterable[Tensor]], Y: Iterable[Iterable[int]],
+                 Z: Iterable[int], type_to_int: Dict[WordType.WordType, int]) -> None:
+        super(TLGDataset, self).__init__()
+        self.X = tuple(map(lambda x: torch.cat([torch.cat(x), torch.zeros([1, 300])]), X))
+        self.Y = tuple(map(lambda x: torch.cat([Tensor([type_to_int['SOS']]), torch.cat(x)]), Y))
+        self.Z = Z  # todo
+        self.type_dict = type_to_int
+
+    def __len__(self) -> int:
+        return len(self.X)
+
+    def __getitem__(self, item: int) -> Tuple[Tensor, Tensor]:
+        return self.X[item], self.Y[item]
---
+++ src/Network.py
@@ -0,0 +1,108 @@
+import sys
+import os
+sys.path.append(os.path.join(os.getcwd(), '..'))
+
+from Transformer.src.Transformer import Transformer, Mask
+
+import torch
+from torch import nn, Tensor
+from torch.utils.data import Dataset
+from torch import optim
+from torch.nn import NLLLoss
+from torch.nn.utils.rnn import pad_sequence
+
+from tqdm import tqdm
+
+import numpy as np
+
+from src import DataPrep
+
+from typing import Callable, List, Any, Tuple
+
+
+def accuracy(predictions: Tensor, truth: Tensor, ignore_idx: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
+    predictions = predictions.argmax(dim=-1)
+    correct_words = torch.ones(predictions.size())
+    correct_words[predictions != truth] = 0
+    correct_words[truth == ignore_idx] = 1
+
+    correct_sentences = correct_words.prod(dim=1)
+    num_correct_sentences = correct_sentences.sum().item()
+
+    num_correct_words = correct_words.sum().item()
+    num_masked_words = len(truth[truth == ignore_idx])
+
+    return (predictions.shape[0], num_correct_sentences), \
+           (predictions.shape[0] * predictions.shape[1] - num_masked_words, num_correct_words - num_masked_words)
+
+
+class Supertagger(nn.Module):
+    def __init__(self, num_classes: int, num_heads: int, encoder_layers: int, decoder_layers: int, d_intermediate: int,
+                 device: str) -> None:
+        super(Supertagger, self).__init__()
+        self.num_classes = num_classes
+        self.embedder = nn.Embedding(num_classes, 300, padding_idx=0, scale_grad_by_freq=True).to(device)
+        self.transformer = Transformer(num_classes=num_classes, output_embedder=self.embedder, num_heads=num_heads,
+                                       encoder_layers=encoder_layers, decoder_layers=decoder_layers,
+                                       d_intermediate=d_intermediate, device=device)
+        self.device = device
+
+    def forward(self, encoder_input: Tensor, decoder_input: Tensor, encoder_mask: Tensor, decoder_mask: Tensor) \
+            -> Tensor:
+        return self.transformer.forward(encoder_input, decoder_input, encoder_mask, decoder_mask)
+
+    def train_epoch(self, dataset: Dataset, batch_size: int, criterion: Callable[[Tensor, Tensor], Tensor],
+                    optimizer: optim.Optimizer, train_indices: List[int]) -> Any:
+        permutation = np.random.permutation(train_indices)
+
+        batch_start = 0
+        loss = 0.
+        BS, BTS, BW, BTW = 0, 0, 0, 0
+
+        while batch_start < len(permutation):
+            optimizer.zero_grad()
+            batch_end = min([batch_start + batch_size, len(permutation)])
+
+            batch_x = [dataset.X[permutation[train_indices[i]]] for i in range(batch_start, batch_end)]
+            batch_y = [dataset.Y[permutation[train_indices[i]]] for i in range(batch_start, batch_end)]
+
+            lens = list(map(len, batch_x))
+
+            batch_x = pad_sequence(batch_x, batch_first=True).to(self.device)
+            batch_y = pad_sequence(batch_y, batch_first=True).long().to(self.device)  # todo
+            batch_e = self.embedder(batch_y).to(self.device)
+
+            encoder_mask = torch.ones(batch_y.shape[0], batch_y.shape[1], batch_y.shape[1])
+            # for i, l in enumerate(lens):
+            #     encoder_mask[i, l::, :] = torch.zeros([1, batch_x.shape[1] - l, batch_x.shape[1]])
+                # encoder_mask[i, :, l::] = torch.zeros([1, batch_x.shape[1], batch_x.shape[1] - l])
+            encoder_mask = encoder_mask.to(self.device)
+            # decoder_mask = torch.ones(batch_y.shape[0], batch_y.shape[1], batch_y.shape[1]).to(self.device)
+            decoder_mask = Mask((batch_y.shape[0], batch_y.shape[1], batch_y.shape[1])).to(self.device)
+            batch_p = self.forward(batch_x, batch_e, encoder_mask, decoder_mask)
+
+            batch_loss = criterion(batch_p[:, :-1].permute(0, 2, 1), batch_y[:, 1:])
+            loss += batch_loss
+            batch_loss.backward()
+            optimizer.step()
+            (bs, bts), (bw, btw) = accuracy(batch_p[:, :-1], batch_y[:, 1:], 0)
+            BS += bs
+            BTS += bts
+            BW += bw
+            BTW += btw
+
+            batch_start += batch_size
+
+        return loss, BS, BTS, BW, BTW
+
+
+def do_everything():
+    tlg = DataPrep.do_everything()
+    num_classes = len(tlg.type_dict) + 1  # todo
+    n = Supertagger(num_classes, 6, 4, 4, 256, 'cuda')
+    L = NLLLoss(ignore_index=0, reduction='sum')
+    o = optim.Adam(n.parameters(), lr=1e-03)
+    for i in range(1000):
+        loss, bs, bts, bw, btw = n.train_epoch(tlg, 256, L, o, list(range(1000)))
+        print('Epoch {}'.format(i))
+        print(' Loss: {}, Sentence Accuracy: {}, Word Accuracy: {}'.format(loss, bts/bs, btw/bw))
