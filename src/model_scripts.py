0424f937533665687bce6b1d7b9d0ad752846428
konstantinosKokos <konstantinos@riseup.net>
2019-03-29 09:17:17 +0100

    better evaluation script

--- src/model_scripts.py
+++ src/model_scripts.py
@@ -5,21 +5,22 @@ from src.supertagger import Supertagger
 import torch
 import numpy as np
 
-from typing import List, Optional
+from typing import List, Tuple
 from itertools import chain
 
 import pickle
 import os
 
-from collections import Counter
+from functools import reduce
+from collections import Counter, defaultdict
 
 snd = lambda x: x[1]
 
 
-def bpe_elmo(data_path='data/XY_atomic_short'):
+def bpe_elmo(data_path='data/XY_50'):
     tlg = dataprep.bpe_elmo(data_path=data_path + '.p')
-    split_path = data_path + '_split.p'
-    store_path = 'stored_models/bpe_elmo.p'
+    split_path = 'data/XY_100_split.p'
+    store_path = 'stored_models/bpe_elmo_50.p'
 
     d_model = 1024
     batch_size = 128
@@ -74,9 +75,9 @@ def bpe_elmo(data_path='data/XY_atomic_short'):
 
 
 def bpe_evaluation():
-    data_path = 'data/XY_atomic_short'
-    split_path = data_path + '_split.p'
-    store_path = 'stored_models/bpe_elmo.p'
+    data_path = 'data/XY_0'
+    split_path = 'data/XY_100_split.p'
+    store_path = 'stored_models/bpe_elmo_0.p'
     result_path = data_path + '_results.tsv'
     new_path = data_path + '_new.tsv'
     data_path = data_path + '.p'
@@ -106,58 +107,107 @@ def bpe_evaluation():
     X_val, Y_val = list(zip(*[(X[i], Y[i]) for i in val_indices]))
     X_test, Y_test = list(zip(*[(X[i], Y[i]) for i in test_indices]))
 
-    def convert_ints_to_types(y: List[int]) -> List[str]:
+    def convert_ints_to_atoms(y: List[int]) -> List[str]:
         return list(map(lambda i: int_to_type[i], y))
 
-    def remove_te(y: List[str]) -> List[str]:
-        return (' '.join(y) + ' ').split(' <TE> ')[:-1]
+    def convert_atoms_to_types(y: List[str]) -> List[str]:
+        return (' '.join(list(map(lambda a: a.replace('+', ' '), y))) + ' ').split(' <TE> ')[:-1]
 
-    def tab_separate(y: List[str]) -> str:
+    def convert_ints_to_types(y: List[int]) -> List[str]:
+        return convert_atoms_to_types(convert_ints_to_atoms(y))
+
+    def tab_join(y: List[str]) -> str:
         return '\t'.join(y)
 
-    def process(y: List[int]) -> str:
-        return tab_separate(remove_te(convert_ints_to_types(y)))
-
-    Y_test_ = list(map(process, Y_test))
-    P_test_ = list(map(process, P_test))
-    Y_train_ = list(map(process, Y_train))
-
-    unique_type_sequences = set(list(chain.from_iterable(map(lambda y: remove_te(convert_ints_to_types(y)), Y_train))))
-
-    def get_unique(y: str, p: str) -> List[str]:
-        return list(map(snd,
-                        list(filter(lambda x: x[1] not in unique_type_sequences, zip(y.split('\t'), p.split('\t'))))))
-
-    def get_unique_counts(y: str, p: str):
-        return Counter(list(map(lambda x: x[0],
-                                list(filter(lambda x: x, list(chain((map(get_unique, y, p)))))))))
-
-    def unique_and_true(y: str, p: str):
-        y = y.split('\t')
-        p = p.split('\t')
-        return list(filter(lambda x: x[1] not in unique_type_sequences and x[0] == x[1], zip(y, p)))
-
-    def all_unique(y: List[str], p: List[str]):
-        return Counter((chain.from_iterable(list(map(get_unique, y, p)))))
-
-    def interleave(X: List[List[str]]) -> List[str]:
-        return [X[j][i] for i in range(len(X[0])) for j in range(len(X))]
-
     X_test_ = list(map(lambda x: list(map(lambda w: w.encode('latin-1', 'replace').decode('latin-1'), x)), X_test))
-    X_test_ = list(map(lambda p: '\t'.join(p), X_test_))
 
-    new_indices = [i for i in range(len(Y_test_)) if get_unique(Y_test_[i], P_test_[i])]
-    X_new_ = [X_test_[i] for i in new_indices]
-    Y_new_ = [Y_test_[i] for i in new_indices]
-    P_new_ = [P_test_[i] for i in new_indices]
-    new_new_ = ['\t'.join(['1' if x not in unique_type_sequences else '0' for x in p.split('\t')])
-                for p in P_new_]
-    XYP_new_ = interleave([X_new_, Y_new_, P_new_, new_new_])
+    Y_test_ = list(map(convert_ints_to_types, Y_test))
+    P_test_ = list(map(convert_ints_to_types, P_test))
+    Y_train_ = list(map(convert_ints_to_types, Y_train))
 
-    XYP_test_ = interleave([X_test_, Y_test_, P_test_])
+    type_counts = Counter(list(chain.from_iterable(Y_train_)))
+
+    def occurrence_correct(y: List[str], p: List[str], occ: int) -> Tuple[int, int]:
+        yp_o = list(filter(lambda yp: type_counts[yp[0]] == occ, zip(y, p)))
+        return len(yp_o), len(list(filter(lambda yp: yp[0] == yp[1], yp_o)))
+
+    def count_occurrence_correct(Y: List[List[str]], P: List[List[str]], occ: int) -> Tuple[int, int]:
+        return reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]), list(map(lambda z, w: occurrence_correct(z, w, occ),
+                                                                        Y, P)))
+
+    def index_occurrence_correct(Y: List[List[str]], P: List[List[str]], occ: int) -> List[int]:
+        yp = list(map(lambda y, p: occurrence_correct(y, p, occ), Y, P))
+        return [i for i in range(len(Y)) if yp[i][1] > 0]
+
+    def imagined(y: List[str], p: List[str], correct: bool=False) -> List[bool]:
+        return [True if (type_counts[p[i]] == 0 and (y[i] == p[i] or not correct)) else False for i in range(len(y))]
+
+    def index_imagined(Y: List[List[str]], P: List[List[str]], correct: bool=False) -> List[int]:
+        C = list(map(lambda y, p: imagined(y, p, correct), Y, P))
+        return [idx for idx, im in enumerate(C) if any(im)]
+
+    def count_imagined(Y: List[List[str]], P: List[List[str]]) -> int:
+        C = list(map(lambda y, p: imagined(y, p), Y, P))
+        return sum([len(list(filter(lambda x: x, im))) for im in C])
+
+    def count_correct(y: List[str], p: List[str]) -> Tuple[int, int]:
+        return len(y), len(list(filter(lambda x: x[0] == x[1], zip(y, p))))
+
+    def accuracy(Y: List[List[str]], P: List[List[str]]) -> Tuple[int, int]:
+        return reduce(lambda x, y: (x[0] + y[0], x[1] + y[1]), list(map(count_correct, Y, P)))
+
+    test_accuracy = accuracy(Y_test_, P_test_)
+    print('Test accuracy: {}/{} ({})'.format(test_accuracy[1], test_accuracy[0], test_accuracy[1]/test_accuracy[0]))
+    for occ in range(11):
+        a = count_occurrence_correct(Y_test_, P_test_, occ)
+        print(' {} Occurrence Accuracy: {}/{} ({})'.format(occ, a[1], a[0], a[1]/a[0]))
+    print('Imagined new types {} times.'.format(count_imagined(Y_test_, P_test_)))
 
     def store(x: List[str], path: str) -> None:
         np.savetxt(path, np.array(x, dtype=object), fmt='%s')
 
-    store(XYP_test_, result_path)
-    store(XYP_new_, new_path)
+    new_indices = index_imagined(Y_test_, P_test_, True)
+    X_new_ = [tab_join(X_test_[i]) for i in new_indices]
+    Y_new_ = [tab_join(Y_test_[i]) for i in new_indices]
+    P_new_ = [tab_join(P_test_[i]) for i in new_indices]
+    # todo
+    new_new_ = [tab_join(list(map(lambda x: '1' if type_counts[x] == 0 else '0', P_test_[i]))) for i in new_indices]
+
+
+
+
+
+    #
+    # def get_unique(y: str, p: str) -> List[str]:
+    #     return list(map(snd,
+    #                     list(filter(lambda x: train_type_counts[x[1]] == 0, zip(y.split('\t'), p.split('\t'))))))
+    #
+    # def get_unique_counts(y: str, p: str):
+    #     return Counter(list(map(lambda x: x[0],
+    #                             list(filter(lambda x: x, list(chain((map(get_unique, y, p)))))))))
+    #
+    # def unique_and_true(y: str, p: str):
+    #     y = y.split('\t')
+    #     p = p.split('\t')
+    #     return list(filter(lambda x: train_type_counts[x[1]] == 0 and x[0] == x[1], zip(y, p)))
+    #
+    # def all_unique(y: List[str], p: List[str]):
+    #     return Counter((chain.from_iterable(list(map(get_unique, y, p)))))
+    #
+    # def interleave(X: List[List[str]]) -> List[str]:
+    #     return [X[j][i] for i in range(len(X[0])) for j in range(len(X))]
+    #
+    #
+    # new_indices = [i for i in range(len(Y_test_)) if get_unique(Y_test_[i], P_test_[i])]
+    # X_new_ = [X_test_[i] for i in new_indices]
+    # Y_new_ = [Y_test_[i] for i in new_indices]
+    # P_new_ = [P_test_[i] for i in new_indices]
+    # new_new_ = ['\t'.join(['1' if x not in unique_type_sequences else '0' for x in p.split('\t')])
+    #             for p in P_new_]
+    # XYP_new_ = interleave([X_new_, Y_new_, P_new_, new_new_])
+    #
+    # XYP_test_ = interleave([X_test_, Y_test_, P_test_])
+    #
+    #
+    # store(XYP_test_, result_path)
+    # store(XYP_new_, new_path)
